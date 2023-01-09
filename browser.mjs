ee119059918bce76de3c4acb6bf921159599abb5
Nate Moore <nate@astro.build>
2023-01-09 14:10:50 -0600

    0.1.3

--- browser.mjs
+++ browser.mjs
@@ -1,12 +1,12 @@
 const ENTITY_RE = /&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-fA-F]{1,6});?/gi;
 
 let p;
-function decodeHTML5Strict(value) {
+function decodeHTML5(value) {
   if (!p) p = new DOMParser();
   return p.parseFromString(value, 'text/html').documentElement.textContent;
 }
 
 export function decode(value) {
   ENTITY_RE.lastIndex = 0;
-  return value.replace(ENTITY_RE, (m) => decodeHTML5Strict(m));
+  return value.replace(ENTITY_RE, (m) => decodeHTML5(m));
 }
